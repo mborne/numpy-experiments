d7e6d4058c229d741b3933b67a71e6617743531a
mborne <mborne@users.noreply.github.com>
2025-05-25 23:48:45 +0200

    remove old sample data for noisy-circle

--- data/noisy-circle.sql
+++
@@ -1,9 +0,0 @@
-create temporary table noisy_circle AS
-	select
-		( 1.0 + r * cos(theta) ) as x,
-		( 2.0 + r * sin(theta) ) as y
-	from (
-		SELECT ( 2.0 * pi() * generate_series(0,1000) / 1000.0 ) as theta, 3.0 + random() / 10.0 as r 
-	) t;
-
-\copy noisy_circle to 'noisy-circle.csv' with CSV HEADER;
